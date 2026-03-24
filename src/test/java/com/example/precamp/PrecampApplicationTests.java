6e408327c82f8fd6ff21bb2a4bddcd045f0c98eb
GolemOnce <shh3350@gmail.com>
2026-03-24 15:56:34 +0900

    initializing

---
+++ src/test/java/com/example/precamp/PrecampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.precamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
