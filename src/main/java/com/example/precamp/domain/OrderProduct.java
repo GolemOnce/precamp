fe5e16e27da5d281873d385ffefb290febb99216
GolemOnce <shh3350@gmail.com>
2026-03-27 17:14:02 +0900

    Add comments for OrderProduct entity relationship
    Added comments explaining the relationship between Order and Product.

--- src/main/java/com/example/precamp/domain/OrderProduct.java
+++ src/main/java/com/example/precamp/domain/OrderProduct.java
@@ -1,7 +1,9 @@
 package com.example.precamp.domain;
 
 import jakarta.persistence.*;
-
+/* 주문 하나당 상품 하나라 @ManyToOne으로 구현 가능하지만, 실무에선 거의 @ManyToMany일 것이고,
+ OrderProduct를 통해 N+1를 유도해 N+1 문제 예방을 위한 코드 실습하기 위해 중간 테이블 추가함.
+*/
 @Entity
 public class OrderProduct {
 
