f1ffc0ff05fd02dd1274539d50fd5a84e28a5cf4
GolemOnce <shh3350@gmail.com>
2026-03-27 17:19:22 +0900

    상품 레포지토리 구현

---
+++ src/main/java/com/example/precamp/repository/ProductRepository.java
@@ -0,0 +1,36 @@
+package com.example.precamp.repository;
+
+import com.example.precamp.domain.Product;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepository {
+
+    private final EntityManager em;
+
+    public void save(Product product) {
+        if (product.getId() == null) {
+            em.persist(product);
+        } else {
+            em.merge(product);
+        }
+    }
+
+    public void remove(Product product) {
+        em.remove(product);
+    }
+
+    public Product findById(Long id) {
+        return em.find(Product.class, id);
+    }
+
+    public List<Product> findAll() {
+        return em.createQuery("select p from Product p", Product.class).getResultList();
+    }
+
+}
