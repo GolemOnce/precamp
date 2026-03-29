fd1022f7e5e8cec88d840c95ee59da62d08937ba
GolemOnce <shh3350@gmail.com>
2026-03-29 13:42:30 +0900

    N+1방지를 위해 join fetch로 order, orderproduct, product 테이블 한 번에 조회

---
+++ src/main/java/com/example/precamp/repository/OrderRepository.java
@@ -0,0 +1,41 @@
+package com.example.precamp.repository;
+
+import com.example.precamp.domain.Order;
+import com.example.precamp.domain.Product;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findByIdWithProducts(Long orderId) {
+        em.flush();
+        em.clear();
+
+        return em.createQuery(
+                "select o from Order o " +
+                        "join fetch o.orderProducts op " +
+                        "join fetch op.product " +
+                        "where o.id = :orderId", Order.class)
+                .setParameter("orderId", orderId)
+                .getSingleResult();
+    }
+
+    public List<Order> findAllWithProducts() {
+        return em.createQuery(
+                        "select o from Order o " +
+                                "join fetch o.orderProducts op " +
+                                "join fetch op.product", Order.class)
+                .getResultList();
+    }
+}
