01da5380dd1878fbe3d2deba30da7745f9916619
GolemOnce <shh3350@gmail.com>
2026-03-29 13:47:03 +0900

    주문 등록, 단일 조회, 목록 조회 구현

---
+++ src/main/java/com/example/precamp/controller/OrderController.java
@@ -0,0 +1,49 @@
+package com.example.precamp.controller;
+
+import com.example.precamp.domain.Order;
+import com.example.precamp.dto.OrderCreateResponseDto;
+import com.example.precamp.dto.OrderRequestDto;
+import com.example.precamp.dto.OrderResponseDto;
+import com.example.precamp.service.OrderService;
+import com.example.precamp.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final ProductService productService;
+
+    // 등록
+    @PostMapping("/orders/new")
+    public ResponseEntity<OrderCreateResponseDto> save (@RequestBody OrderRequestDto request) {
+        Order order = orderService.saveOrder(request);
+        return ResponseEntity.status(201).body(new OrderCreateResponseDto(order));
+    }
+
+    // 단일 조회
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<OrderResponseDto> findOrderById (@PathVariable Long id) {
+        Order order = orderService.findOrderById(id);
+        return ResponseEntity.ok(new OrderResponseDto(order));
+    }
+
+
+    // 목록 조회(도전)
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponseDto>> findAllOrders() {
+        List<OrderResponseDto> orders = orderService.findAllOrders().stream()
+                .map(OrderResponseDto::new).toList();
+        return ResponseEntity.ok(orders);
+    }
+}
